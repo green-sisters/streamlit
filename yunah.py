a10133003f2d48ab176da39f34c2af36262b6bc5
최윤아 <113593461+yunah0306@users.noreply.github.com>
2023-06-16 17:40:22 +0900

    Update yunah.py

--- yunah.py
+++ yunah.py
@@ -2,19 +2,23 @@ import streamlit as st
 import pandas as pd
 import numpy as np
 import os
+import io
 from datetime import datetime
 import platform
 from PIL import ImageFont, ImageDraw, Image
-# from matplotlib import pyplot as plt
-import uuid
-import json
 import time
-#import cv2
-import requests
 import re
+import tempfile
+# from matplotlib import pyplot as plt
+# import cv2
+
+import requests
+import uuid
+import time
+import json
 
 # OCR 인식 함수
-def OCR(upload_file):
+def extract_text(file):
   api_url = 'https://8cbdvua55p.apigw.ntruss.com/custom/v1/22878/18b029032c74f9dc3223fcfe629227edf8e8880be05ecf50148ea52dae003f79/general'
   secret_key = 'R1hjd3JEam9pT3ZRTmRNRkxPTG9MVWhqanpxQmRoeHk='
 
@@ -32,7 +36,7 @@ def OCR(upload_file):
 
   payload = {'message': json.dumps(request_json).encode('UTF-8')}
   files = [
-    ('file', upload_file)
+    ('file',open(file,'rb'))
   ]
   headers = {
     'X-OCR-SECRET': secret_key
@@ -44,7 +48,7 @@ def OCR(upload_file):
   # 정규표현식 패턴에 매칭되는 모든 한글 단어를 추출하여 리스트에 저장
   hangul_words = hangul_pattern.findall(response.text)
   
-  word_choice = ['다회용기','개인컵','다회용컵','에코별']
+  word_choice = ['다회용기','개인컵','다회용컵','컵할인']
 
   count = 0
   used = []
@@ -55,7 +59,7 @@ def OCR(upload_file):
             used.append(target_word)
   setence = ', '.join(used)
   point = 10 * count
-  print(setence,'을(를) 사용하셨군요!',f'{point}포인트가 지급되었습니다!')
+  st.text(f"{setence}을(를) 이용하셨군요! {point}포인트가 지급되었습니다!")
   
   
   
@@ -75,11 +79,14 @@ if option == '영수증 적립':
     upload_file = st.file_uploader('사진을 업로드 해주세요', type=['jpg', 'png', 'jpeg'])
     if upload_file is not None:
       # 이미지 열기
-      image = Image.open(upload_file)
-      image = image.resize((256,512))
-      st.image(image)
+      img = Image.open(upload_file)
+      img = img.resize((256,512))
+      st.image(img)
       # OCR
-      OCR(upload_file)
+      with tempfile.NamedTemporaryFile(delete=False, suffix=os.path.splitext(upload_file.name)[1]) as temp_file:
+        img.save(temp_file.name,)
+        extract_text(temp_file.name)
+
       
       
         
