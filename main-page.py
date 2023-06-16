1b1e52b7bdacb788ecd199afe04db2e08cca9c41
chaexnxnii <113587113+chaexnxnii@users.noreply.github.com>
2023-06-16 20:33:35 +0900

    Update main-page.py

--- main-page.py
+++ main-page.py
@@ -23,31 +23,52 @@
 # st.title('This is a title')
 # st.write('This is some content.')
 
-import { transparentize } from "color2k"
-import { colors } from "src/theme/primitives/colors"
+from streamlit.theme import Theme
 
-export default {
-  ...colors,
-  bgColor: colors.white,
-  secondaryBg: colors.gray20,
-  bodyText: colors.gray85,
-  warning: colors.yellow110,
-  warningBg: transparentize(colors.yellow80, 0.8),
-  success: colors.green100,
-  successBg: transparentize(colors.green80, 0.8),
-  infoBg: transparentize(colors.blue70, 0.9),
-  info: colors.blue100,
-  danger: colors.red100,
-  dangerBg: transparentize(colors.red70, 0.8),
-
-  primary: colors.red70,
-  disabled: colors.gray40,
-  lightestGray: colors.gray20,
-  lightGray: colors.gray30,
-  gray: colors.gray60,
-  darkGray: colors.gray70,
-  red: colors.red80,
-  blue: colors.blue80,
-  green: colors.green80,
-  yellow: colors.yellow80,
+colors = {
+    "white": "#ffffff",
+    "gray20": "#333333",
+    "gray30": "#666666",
+    "gray60": "#999999",
+    "gray70": "#b3b3b3",
+    "gray85": "#d9d9d9",
+    "red70": "#ff4b4b",
+    "blue70": "#4b4bff",
+    "blue80": "#3333ff",
+    "green80": "#33cc33",
+    "yellow80": "#cccc00",
+    "yellow110": "#ffcc00",
+    "red100": "#ff0000",
+    "red80": "#ff3333",
+    "green100": "#00ff00",
+    "green80": "#33cc33",
+    "blue100": "#0000ff"
 }
+
+theme = Theme(
+    bgColor=colors["white"],
+    secondaryBg=colors["gray20"],
+    bodyText=colors["gray85"],
+    warning=colors["yellow110"],
+    warningBg=colors["yellow80"],
+    success=colors["green100"],
+    successBg=colors["green80"],
+    infoBg=colors["blue70"],
+    info=colors["blue100"],
+    danger=colors["red100"],
+    dangerBg=colors["red80"],
+    primary=colors["red70"],
+    disabled=colors["gray60"],
+    lightestGray=colors["gray20"],
+    lightGray=colors["gray30"],
+    gray=colors["gray60"],
+    darkGray=colors["gray70"],
+    red=colors["red80"],
+    blue=colors["blue80"],
+    green=colors["green80"],
+    yellow=colors["yellow80"]
+)
+
+# Streamlit 앱 설정
+st.set_theme(theme)
+
