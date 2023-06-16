5726a3b1bc056e2ab1d8044ef3be26270ebbf751
최윤아 <113593461+yunah0306@users.noreply.github.com>
2023-06-17 00:23:52 +0900

    Update yunah.py

--- yunah.py
+++ yunah.py
@@ -9,7 +9,7 @@ from PIL import ImageFont, ImageDraw, Image
 import time
 import re
 import tempfile
-# from matplotlib import pyplot as plt
+from matplotlib import pyplot as plt
 # import cv2
 
 import requests
@@ -17,7 +17,9 @@ import uuid
 import time
 import json
 
-# OCR 인식 함수
+from tensorflow.keras.models import load_model
+
+## OCR 인식 함수 ##
 def extract_text(file):
   api_url = 'https://8cbdvua55p.apigw.ntruss.com/custom/v1/22878/18b029032c74f9dc3223fcfe629227edf8e8880be05ecf50148ea52dae003f79/general'
   secret_key = 'R1hjd3JEam9pT3ZRTmRNRkxPTG9MVWhqanpxQmRoeHk='
@@ -62,8 +64,42 @@ def extract_text(file):
   st.text(f"{setence}을(를) 이용하셨군요! {point}포인트가 지급되었습니다!")
   
   
+
+## 쓰레기 인식 함수 ##
+def classification(image):
+  model_path = 'model_2'
+
+  model = load_model(model_path)
   
-  
+  # 예측
+  f = image
+  category = os.listdir('dataset')
+
+  image_w = 64
+  image_h = 64
+
+  pixels = image_h * image_w * 3
+
+  x = []
+  filenames = []
+
+  img = Image.open(f)
+  img = img.convert("RGB")
+  img = img.resize((image_w, image_h))
+  data = np.asarray(img)
+  filenames.append(f)
+  x.append(data)
+  x = np.array(x)
+  prediction_test = model.predict(x)
+
+  file_index = 0
+  result = []
+  for i in prediction_test:
+      label = i.argmax() # [0.000, 0.000, 0.000, ..., 0.000, 1.000, 0.000] 중 최대값 추출 즉,1값의 인덱스
+      if category[label] == '확인불가':
+        st.text('확인이 불가합니다. 올바르게 배출해주세요')
+      else:
+        st.text(f'{category[label]}을 배출하셨습니다. 포인트가 지급되었습니다!')
   
   
   
@@ -91,7 +127,16 @@ if option == '영수증 적립':
       
         
 if option == '쓰레기 배출':
-  trash_option = st.selectbox('어떤 종류의 쓰레기를 배출하나요?',
-                       ('유리','캔','플라스틱'))
-  # trash_option이랑 모델 결과가 같으면 
-  # trash_option을 배출했어요. 포인트는 ~~
+  #trash_option = st.selectbox('어떤 종류의 쓰레기를 배출하나요?',
+                       #('유리','캔','플라스틱'))
+
+  upload_file = st.file_uploader('이미지를 인식합니다.', type=['jpg', 'png', 'jpeg'])
+  if upload_file is not None:
+    # 이미지 열기
+    img = Image.open(upload_file)
+    img = img.resize((256,256))
+    st.image(img)
+    # 이미지 인식
+    with tempfile.NamedTemporaryFile(delete=False, suffix=os.path.splitext(upload_file.name)[1]) as temp_file:
+      img.save(temp_file.name,)
+      classification(temp_file.name)
