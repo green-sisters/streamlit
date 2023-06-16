0fdab1b656fdf4f3708a3d7b5f7c357189fbbb0f
chaexnxnii <113587113+chaexnxnii@users.noreply.github.com>
2023-06-16 19:43:49 +0900

    Update 1. 메인 페이지.py

--- 1. 메인 페이지.py
+++ 1. 메인 페이지.py
@@ -2,21 +2,14 @@
 # 색상: rgba(102, 234, 0, 0.73) (=#66ea00;), 크기 조정
 
 import streamlit as st
-st.markdown(
-    """
-    <style>
-    body {
-        background: linear-gradient(rgba(102, 234, 0, 0.73), rgba(102, 234, 0, 0.03));
-        background-size: cover;
-    }
-    </style>
-    """,
-    unsafe_allow_html=True
-)
 
 st.markdown(
     """
     <style>
+    .reportview-container {
+        background: linear-gradient(rgba(102, 234, 0, 0.73), rgba(102, 234, 0, 0.03));
+        background-size: cover;
+    }
     @media (max-width: 600px) {
         .reportview-container .markdown-text-container {
             font-size: 16px;
@@ -29,3 +22,4 @@ st.markdown(
 
 st.title('This is a title')
 st.write('This is some content.')
+
