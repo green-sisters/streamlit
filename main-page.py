d7a767decd337ff8f8b6cb092c9a07e4e5058c24
chaexnxnii <113587113+chaexnxnii@users.noreply.github.com>
2023-06-16 20:32:13 +0900

    Update main-page.py

--- main-page.py
+++ main-page.py
@@ -23,47 +23,31 @@
 # st.title('This is a title')
 # st.write('This is some content.')
 
-preset_colors = [
-    ("Default light", ThemeColor(
-        primaryColor="#ff4b4b",
-        backgroundColor="#ffffff",
-        secondaryBackgroundColor="#f0f2f6",
-        textColor="#31333F",
-    )),
-    ("Default dark", ThemeColor(
-        primaryColor="#ff4b4b",
-        backgroundColor="#0e1117",
-        secondaryBackgroundColor="#262730",
-        textColor="#fafafa",
-    )),
-    ("Custom theme", ThemeColor(
-        primaryColor="rgba(102, 234, 0, 0.73)",
-        backgroundColor="rgba(102, 234, 0, 0.73)",
-        secondaryBackgroundColor="rgba(102, 234, 0, 0.73)",
-        textColor="rgba(102, 234, 0, 0.73)",
-    )),
-]
+import { transparentize } from "color2k"
+import { colors } from "src/theme/primitives/colors"
 
-def get_config_theme_color():
-    config_theme_primaryColor = st._config.get_option('theme.primaryColor')
-    config_theme_backgroundColor = st._config.get_option('theme.backgroundColor')
-    config_theme_secondaryBackgroundColor = st._config.get_option('theme.secondaryBackgroundColor')
-    config_theme_textColor = st._config.get_option('theme.textColor')
-    if (
-        config_theme_primaryColor is not None and
-        config_theme_backgroundColor is not None and
-        config_theme_secondaryBackgroundColor is not None and
-        config_theme_textColor is not None
-    ):
-        return ThemeColor(
-            primaryColor=config_theme_primaryColor,
-            backgroundColor=config_theme_backgroundColor,
-            secondaryBackgroundColor=config_theme_secondaryBackgroundColor,
-            textColor=config_theme_textColor,
-        )
+export default {
+  ...colors,
+  bgColor: colors.white,
+  secondaryBg: colors.gray20,
+  bodyText: colors.gray85,
+  warning: colors.yellow110,
+  warningBg: transparentize(colors.yellow80, 0.8),
+  success: colors.green100,
+  successBg: transparentize(colors.green80, 0.8),
+  infoBg: transparentize(colors.blue70, 0.9),
+  info: colors.blue100,
+  danger: colors.red100,
+  dangerBg: transparentize(colors.red70, 0.8),
 
-    return None
-
-theme_from_initial_config = get_config_theme_color()
-if theme_from_initial_config:
-    preset_colors.append(("From the config", theme_from_initial_config))
+  primary: colors.red70,
+  disabled: colors.gray40,
+  lightestGray: colors.gray20,
+  lightGray: colors.gray30,
+  gray: colors.gray60,
+  darkGray: colors.gray70,
+  red: colors.red80,
+  blue: colors.blue80,
+  green: colors.green80,
+  yellow: colors.yellow80,
+}
