989d3019c3600b767d276c58fae568d834e57ee5
chaexnxnii <113587113+chaexnxnii@users.noreply.github.com>
2023-06-18 00:45:39 +0900

    Update receipt-page.py

--- receipt-page.py
+++ receipt-page.py
@@ -17,7 +17,53 @@ secret_key = 'dVdKZmpqSWpRUnh3c3d2Vmd1ZE1tVVhZbXd4cVdVVnU='
 
 def main():
     st.title("Ecollege")
-    # ...
+    #b.영수증 종류 선택
+    option2 = st.selectbox(
+        '영수증 종류를 선택해주세요.',
+        ('전자영수증', '실물영수증')
+    )
+#c. 전자영수증일 경우 캡쳐한 파일을 업로드, 실물영수증일 경우 직접 촬영, 그 후 다회용기를 사용했을 경우 10포인트(예시) 적립
+    if option2 == '전자영수증':
+        st.write('영수증을 불러와주세요.')
+        uploaded_file = st.file_uploader('', type=["jpg", "png"])
+        if uploaded_file is not None:
+            image = Image.open(uploaded_file)
+            st.image(image, caption='', use_column_width=True)
+            image_bytes = uploaded_file.getvalue()
+            text = ocr_naver(image_bytes)
+            if '개인 컵' in text:
+                add_points(10) 
+                st.write('10포인트가 적립되었습니다.')
+    
+    elif option2 == '실물영수증':
+        st.write('사진 촬영하러 가기')
+        
+        # 이미지 촬영
+        capture = cv2.VideoCapture(0)
+        ret, frame = capture.read()
+        if ret:
+            image = Image.fromarray(frame)
+            st.image(image, caption='', use_column_width=True)
+            
+            # 촬영된 이미지를 임시 파일로 저장
+            temp_file = tempfile.NamedTemporaryFile(delete=False)
+            temp_file.close()
+            image.save(temp_file.name)
+            
+            # 저장된 이미지 파일을 업로드한 후 포인트 
+            uploaded_file = st.file_uploader('', type=["jpg", "png"], accept_multiple_files=False)
+            if uploaded_file is not None:
+                uploaded_image = Image.open(uploaded_file)
+                st.image(uploaded_image, caption='', use_column_width=True)
+                image_bytes = uploaded_file.getvalue()
+                text = ocr_naver(image_bytes)
+                if '개인 컵' in text:
+                    add_points(10) 
+                    st.write('10포인트가 적립되었습니다.')
+        
+        capture.release()
+        cv2.destroyAllWindows()
+        
     st.container()
     st.markdown("""
         <div style="background-color: #f6f5d0; color: #000000; padding: 10px;">
