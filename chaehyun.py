c0b87ecb963af7e329f7b90754d6014c7e800c97
chaexnxnii <113587113+chaexnxnii@users.noreply.github.com>
2023-06-19 20:35:30 +0900

    Update chaehyun.py

--- chaehyun.py
+++ chaehyun.py
@@ -345,7 +345,7 @@ if option2 == '사용 가능한 매장 보러가기':
             st.image(img6, caption='컴포즈')
       
       
-    if st.session_state['campus'] == '대학교':
+    if st.session_state['campus'] == '연세대학교':
         st.subheader(f"{campus}에서 사용 가능한 매장입니다")
         st.write("")
         img1 = Image.open('src/안내 사진/고를샘.png')
@@ -366,7 +366,7 @@ if option2 == '사용 가능한 매장 보러가기':
           st.image(img3, caption='부를샘')
           st.image(img4, caption='하얀샘')
         
-    if st.session_state['campus'] == 자이화여자대학교':
+    if st.session_state['campus'] == '이화여자대학교':
         st.subheader(f"{campus}에서 사용 가능한 매장입니다")
         st.write("")
         img1 = Image.open('안내 사진/닥터로빈.png')
