32309c85abef04a2a937fb2258ad8cde0ffdf03c
chaexnxnii <113587113+chaexnxnii@users.noreply.github.com>
2023-06-19 20:05:32 +0900

    Update chaehyun.py

--- chaehyun.py
+++ chaehyun.py
@@ -147,13 +147,13 @@ if option0 == '대학교 인증하기':
         st.session_state['campus'] = ''
 
     user_name = st.text_input("이름을 입력하세요", value=st.session_state['user_name'])
-    campus_list = ['서강대학교', '연세대학교', '이화여자대학교', '홍익대학교']
 
     if user_name:
         st.session_state['user_name'] = user_name
         st.sidebar.text(f'{user_name}님, Ecollege에 오신걸 환영합니다!')
 
-        campus = st.radio('재학중인 학교를 선택하세요', campus_list, index=0 if st.session_state['campus'] == '' else campus_list.index(st.session_state['campus']))
+        campus_list = ['서강대학교', '연세대학교', '이화여자대학교', '홍익대학교']
+        campus = st.selectbox('재학중인 학교를 선택하세요', campus_list, index=0 if st.session_state['campus'] == '' else campus_list.index(st.session_state['campus']))
 
         if campus != st.session_state['campus']:
             st.session_state['campus'] = campus
